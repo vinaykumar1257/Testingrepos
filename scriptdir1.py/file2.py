29ee6b15e5754d6a0479909337e814561ac0bd30
vinaykumar1257 <vinaykumar1257@gmail.com>
2019-11-21 17:23:38 +0530

    devops new 1

---
+++ scriptdir1.py/file2.py
@@ -0,0 +1,9 @@
+devops is having ansible nexus git maven all are under devops
+ansible is having modules
+nexus is for storing artifacts
+git is version control system
+maven is for Build tool
+jenkins is for continues integration pipeline
+
+linux basic commands also cover in this devops
+vagrant also under devops category
