8e123fadfde40ec92b5e42e53b5caf04a80aecfc
vinaykumar1257 <vinaykumar1257@gmail.com>
2019-11-21 17:36:46 +0530

    updated bus strike info..

--- file10.py
+++ file10.py
@@ -1,2 +1,4 @@
 Hyd Bus stirke is started one month ago still that bus strike will continue
 people are travelling on cabs,autos,metros.
+people are little frastated
+therie no updation about complete this bus strike.
