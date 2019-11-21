a0077be438e951c98c93f270a2e389e36af37295
vinaykumar1257 <vinaykumar1257@gmail.com>
2019-11-21 17:14:22 +0530

    devops desc

---
+++ file2.py
@@ -0,0 +1,6 @@
+devops is having ansible nexus git maven all are under devops
+ansible is having modules
+nexus is for storing artifacts
+git is version control system
+maven is for Build tool
+jenkins is for continues integration pipeline
